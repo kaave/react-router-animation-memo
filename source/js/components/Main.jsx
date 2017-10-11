458179cf5e5646b51d0c2de61b1d70a74d0773d4
kaave <abe@framelunch.jp>
2017-10-11 22:31:19 +0900

    [Case 1] Simple slide

--- source/js/components/Main.jsx
+++ source/js/components/Main.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import routes from '../routes';
 
 export default withRouter(({ location }) => (
-  <main id="main" className="main" role="main">
-    <Switch {...{ location }}>
-      {routes.map(({ path, component, exact }) => (
-        <Route key={path} {...{ path, exact, component }} />
-      ))}
-    </Switch>
-  </main>
+  <TransitionGroup
+    component="main"
+    id="main"
+    className="main"
+    role="main"
+  >
+    <CSSTransition
+      key={location.pathname}
+      classNames="route-change-animate"
+      timeout={800}
+      appear
+    >
+      <Switch {...{ location }}>
+        {routes.map(({ path, exact, component }) => (
+          <Route key={path} {...{ path, exact, component }} />
+        ))}
+      </Switch>
+    </CSSTransition>
+  </TransitionGroup>
 ));
